3113ebb5e6ac8d9319b2b4fbd888589186b301c7
Leslie Qin <61098061+Lqob7@users.noreply.github.com>
2024-02-21 10:35:17 -0700

    Revert "updated kid level test"

--- LifeGoesOn/Assets/Scripts/NoteObject.cs
+++ LifeGoesOn/Assets/Scripts/NoteObject.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
-	public bool canBePressed;
-    
-	public KeyCode keyToPress;
-    public GameObject player;
-
-	private float distanceBetweenObjects;
-
-	public Animator anim; 
+    public bool canBePressed;
+    public KeyCode keyToPress;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -24,57 +18,25 @@ public class NoteObject : MonoBehaviour
         if(Input.GetKeyDown(keyToPress))
         {
             if(canBePressed)
-            {	
-	            anim.Play("Beat");	
-                distanceBetweenObjects = transform.position.x - player.transform.position.x;
-				//gameObject.SetActive(false);
-                
-                // if the x distance between the player and the note is less than 0.5, then it's a perfect hit
-                if (Mathf.Abs(distanceBetweenObjects) < 0.25)
-                {
-                    
-					anim.Play("Perfect");
-					Debug.Log("Perfect Hit"); // replace with perfect hit animation
-                    //currentScore += scorePerPerfectNote;
-	
-					// after the animation is done, set the gameobject to inactive
-					
-                } 
-                else if (Mathf.Abs(distanceBetweenObjects) < 0.5)
-                {
-                    anim.Play("Good");
-					//gameObject.SetActive(false);
-					Debug.Log("Good Hit"); // replace with good hit animation
-                    //currentScore += scorePerGoodNote;
-                } else
-                {
-	                anim.Play("OK");	
-                    //gameObject.SetActive(false);
-					Debug.Log("OK Hit"); // replace with ok hit animation
-                    //currentScore += scorePerOKNote;
-                }
-            } 
+            {
+                gameObject.SetActive(false);
+            }
         }
+        
     }
-    
-    // detect if the player object is in the hitbox area, if so, player can press the key
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Activator")
         {
             canBePressed = true;
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Activator")
         {
             canBePressed = false;
-            Debug.Log("Missed Note"); // replace with missed note animation
-			//
-			
-	
-			
+            //GameManager.instance.NoteMissed();
         }
     }
 }
