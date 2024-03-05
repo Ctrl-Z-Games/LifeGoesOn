0e184105eb1859f572e17ec1b8b912637c4a69af
azahin1 <azahin1@ualberta.ca>
2024-03-05 12:54:05 -0700

    fix text so it appears in build version

--- LifeGoesOn/Assets/Scripts/placeDialogue.cs
+++ LifeGoesOn/Assets/Scripts/placeDialogue.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 
 public class placeDialogue : MonoBehaviour
 {
-    public string folderPath;
-    public string levelName;
+    public List<Sprite> dialogs;
     public GameObject spritePrefab;
     public float space;
 
     // Start is called before the first frame update
     void Start() {
-        int numDialogues = Directory.GetFiles(folderPath, "*.png").Length;
-
-        for (int i = 1; i <= numDialogues; i++) {
-            Texture2D texture = new(2, 2); // dummy value
-            byte[] fileData = File.ReadAllBytes(Path.Combine(folderPath, levelName + "Text" + i.ToString()) + ".png");
-            texture.LoadImage(fileData);
-
+        for (int i = 0; i < dialogs.Count; i++) {
             GameObject text = Instantiate(spritePrefab, transform);
-            text.transform.position = new Vector3(space * i, 2.5f);
-            text.GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            text.transform.position = new Vector3(space * (i + 1), 2.5f);
+            text.GetComponent<SpriteRenderer>().sprite = dialogs[i];
         }
     }
 }
