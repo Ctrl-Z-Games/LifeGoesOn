2bada74b0f6450f5caeb0f4686d37156048fc772
Lqob7 <61098061+Lqob7@users.noreply.github.com>
2024-04-05 14:07:57 -0600

    added updated senior bg

--- LifeGoesOn/Assets/Scripts/placeDialogue.cs
+++ LifeGoesOn/Assets/Scripts/placeDialogue.cs
@@ -11,14 +11,18 @@ public class placeDialogue : MonoBehaviour
     public float speed = 0.005f;
     public float offset = 0.0f;
     public bool enabled = false;
+	public float height = 1f;
     private List<GameObject> texts;
 
     // Start is called before the first frame update
     void Start() {
         texts = new List<GameObject>();
+		// timer = delayTime;
         for (int i = 0; i < dialogs.Count; i++) {
             GameObject text = Instantiate(spritePrefab, transform);
             text.transform.position = new Vector3(space * (i + 1) + offset, 2.5f);
+			text.transform.position = new Vector3(space * (i + 1) + offset, height, 0f);
+			//text.transform.position = new Vector3(text.transform.position.x, height, text.transform.position.z);
             text.GetComponent<SpriteRenderer>().sprite = dialogs[i];
             texts.Add(text);
         }
