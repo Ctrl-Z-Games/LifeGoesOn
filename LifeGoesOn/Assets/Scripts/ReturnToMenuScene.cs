d233d93bfc6ba7644dcdb6d5154d91c0343ae26b
Lqob7 <61098061+Lqob7@users.noreply.github.com>
2024-04-12 22:33:01 -0600

    update credit scene
    after credit scene back to main menu

---
+++ LifeGoesOn/Assets/Scripts/ReturnToMenuScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ReturnToMenuScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the Enter key is pressed
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
