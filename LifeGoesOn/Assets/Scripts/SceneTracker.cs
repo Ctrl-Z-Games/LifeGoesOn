789a289a7248a3b05e82d8043d03fa2d22493ba3
Lqob7 <61098061+Lqob7@users.noreply.github.com>
2024-04-14 16:26:55 -0600

    fix ending scene

--- LifeGoesOn/Assets/Scripts/SceneTracker.cs
+++ LifeGoesOn/Assets/Scripts/SceneTracker.cs
@@ -6,8 +6,47 @@ using UnityEngine.SceneManagement;
 public class SceneTracker : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int happy;
+    public int neutral;
+    public int sad;
+    public Animator animator;
+    public HashSet<int> playedScenes = new HashSet<int>();
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
     void Start()
     {
-        //GameManager.instance.RecordScenePlayed(SceneManager.GetActiveScene().buildIndex);
+        animator = GetComponent<Animator>(); 
+        RecordScenePlayed();
+        
+        if(playedScenes.Contains(happy))
+        {
+            animator.Play("Happy");
+        }
+        else if(playedScenes.Contains(neutral))
+        {
+            animator.Play("Neutral");
+        }
+        else if(playedScenes.Contains(sad))
+        {
+            animator.Play("Sad");
+        }
     }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        RecordScenePlayed();
+    }
+    public void RecordScenePlayed()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        playedScenes.Add(sceneIndex);
+        Debug.Log("Scene " + sceneIndex + " has been played");
+        
+    }
+    
 }
