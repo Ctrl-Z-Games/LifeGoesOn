fe5529405dc09cb7fb0dd50283231c110c4edd43
azahin1 <z4hin.4brar@gmail.com>
2025-05-22 21:24:20 -0600

    add canvas and text display starting in baby level

---
+++ LifeGoesOn/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour {
+    public static ScoreDisplay instance;
+    private TextMeshProUGUI scoreDisp;
+    [SerializeField] private Color scoreColor;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        scoreDisp = GetComponent<TextMeshProUGUI>();
+        scoreDisp.color = scoreColor;
+        scoreDisp.text = "Score: 0";
+    }
+
+    public void UpdateScore(int score) {
+        scoreDisp.text = "Score: " + score.ToString();
+    }
+}
