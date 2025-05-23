53097a4c11867fc70e362a4730447017af7c6fa9
azahin1 <z4hin.4brar@gmail.com>
2025-05-22 21:38:09 -0600

    make it not a singleton, because game manager is a singleton

--- LifeGoesOn/Assets/Scripts/ScoreDisplay.cs
+++ LifeGoesOn/Assets/Scripts/ScoreDisplay.cs
@@ -2,18 +2,8 @@ using TMPro;
 using UnityEngine;
 
 public class ScoreDisplay : MonoBehaviour {
-    public static ScoreDisplay instance;
     private TextMeshProUGUI scoreDisp;
     [SerializeField] private Color scoreColor;
-
-    private void Awake() {
-        if (instance == null) {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        } else {
-            Destroy(gameObject);
-        }
-    }
 
     private void Start() {
         scoreDisp = GetComponent<TextMeshProUGUI>();
