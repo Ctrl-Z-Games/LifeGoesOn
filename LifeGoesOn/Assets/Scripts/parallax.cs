b272b636186ac266b4434a5010ff7c0c0fb1c0ab
azahin1 <z4hin.4brar@gmail.com>
2025-06-05 22:06:25 -0600

    fix parallax effect for baby bc there was a visual bug in that level

--- LifeGoesOn/Assets/Scripts/parallax.cs
+++ LifeGoesOn/Assets/Scripts/parallax.cs
@@ -6,19 +6,18 @@ public class parallax : MonoBehaviour
 {
     public GameObject gameCamera;
     public float parallaxAmount;
-    private float length, startPos, count;
+    private float length, startPos;
 
     // Start is called before the first frame update
     void Start() {
         startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
-        count = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         transform.position = new Vector3(startPos + gameCamera.transform.position.x * parallaxAmount, // add parallax effect
-                                         transform.position.y + parallaxAmount * 0.01f * Mathf.Sin(count),
+                                         transform.position.y + parallaxAmount * 0.01f * Mathf.Sin(10.0f * Time.deltaTime),
                                          //transform.position.y, // if virtical bobbing gets too much, use this instead
                                          transform.position.z);
         ;
@@ -26,6 +25,5 @@ public class parallax : MonoBehaviour
         float loopCheck = gameCamera.transform.position.x * (1 - parallaxAmount); // check for looping
         if (loopCheck > startPos + length) { startPos += length; }
         else if (loopCheck < startPos - length) { startPos -= length; }
-        count += 0.05f;
     }
 }
