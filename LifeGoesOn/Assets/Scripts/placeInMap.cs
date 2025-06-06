3d7fa141bfef856b653bfed826ae3d443cda6667
azahin1 <z4hin.4brar@gmail.com>
2025-06-06 14:26:25 -0600

    connect new map format to one map

--- LifeGoesOn/Assets/Scripts/placeInMap.cs
+++ LifeGoesOn/Assets/Scripts/placeInMap.cs
@@ -7,32 +7,30 @@ public class placeInMap : MonoBehaviour
     public GameObject qHitbox, pHitbox;
 
     private GameObject obj;
-    private string map = "0";
 
     // Start is called before the first frame update
     void Start() {
-        map = mapFile.text; // read from txt file
+        string[] map = mapFile.text.Split(new[] { "\r", "\n"}, System.StringSplitOptions.RemoveEmptyEntries); // read from txt file
         for (int i = 0; i < map.Length; i++) {
-            int mapCode = int.Parse(map[i].ToString());
-            switch (mapCode) { // instantiates new hitbox and set it's position
-                case 0:
+            switch (map[i].Trim()) { // instantiates new hitbox and set it's position
+                case "00":
                     break;
 
-                case 1: // bottom Q
+                case "10": // bottom Q
                     obj = Instantiate(qHitbox, transform);
                     obj.layer = 3;
                     obj.transform.position = transform.position + new Vector3(i * horizontalSpaceMod, 0.4f);
                     obj.AddComponent<NoteObject>().noteType = 1;
                     break;
 
-                case 2: // top P
+                case "01": // top P
                     obj = Instantiate(pHitbox, transform);
                     obj.layer = 3;
                     obj.transform.position = transform.position + new Vector3(i * horizontalSpaceMod, 2.0f);
                     obj.AddComponent<NoteObject>().noteType = 2;
                     break;
 
-                case 3: // both Q + P
+                case "11": // both Q + P
                     obj = Instantiate(qHitbox, transform);
                     obj.layer = 3;
                     obj.transform.position = transform.position + new Vector3(i * horizontalSpaceMod, 0.4f);
