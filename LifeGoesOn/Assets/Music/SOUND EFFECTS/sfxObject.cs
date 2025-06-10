35417cb217dd286b9df5c982a4db313e4c3f9129
azahin1 <z4hin.4brar@gmail.com>
2025-06-10 13:58:40 -0600

    make scriptable object for sfx

---
+++ LifeGoesOn/Assets/Music/SOUND EFFECTS/sfxObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class sfxObject : ScriptableObject {
+    public AudioClip generalClick;
+    public AudioClip failSound;
+    public AudioClip okSound;
+    public AudioClip goodSound;
+    public AudioClip perfectSound;
+    public AudioClip startSound;
+}
